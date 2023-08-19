7fb6edc4eb0c42daa507a90bfbfc5f227040674d
ZangTengfei <tengfei.zang@compass.com>
2023-08-20 01:31:33 +0800

    NiceModal.Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
+import NiceModal from "@ebay/nice-modal-react";
 import { AppProps } from "next/app";
 
 import "../styles/global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <NextUIProvider>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </NextUIProvider>
+    <NiceModal.Provider>
+      <NextUIProvider>
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </NextUIProvider>
+    </NiceModal.Provider>
   );
 };
 
